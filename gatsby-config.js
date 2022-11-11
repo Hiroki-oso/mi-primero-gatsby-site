523c62e2f171bd8749f1b811fdfe7a52ba40ffcd
sonrisahy <sonrisa@cap.ocn.ne.jp>
2022-11-11 21:36:52 +0900

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Mi Primero Gatsby Site`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
